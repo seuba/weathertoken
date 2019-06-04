3929269ff3197dde1b8c5c40120753c722ab8912
seuba <49449750+seuba@users.noreply.github.com>
2019-06-04 14:47:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,37 @@
   <body>
     
 	  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js"></script> 
-	
-	  <script type="text/javascript">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	  
+<?php
+/********************************************************/
+/*			Control the weather V1.0					*/
+/*			By Albert Seuba	- 042319					*/
+/********************************************************/
+/* Consultamos el tiempo actual (ciudad=Barcelona), 
+podemos variar la url pasando una variable desde inArguments y transformando country a su código */
+/*$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://dataservice.accuweather.com/currentconditions/v1/307297?apikey=aE0Mu6wczdfgTIZacsEksP0KBDAUYZjr",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+	$tempsbarcelona = json_decode($response);
+	$accuweather_temps = $tempsbarcelona[0]->{'WeatherText'};
+	echo $accuweather_temps;
+};*/
+	  echo 'sunny';
+?>
+<script type="text/javascript">
 		/*
  * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
@@ -11729,33 +11758,5 @@ if (window.self === window.top) {
 	});
 }
 		</script>
-<?php
-/********************************************************/
-/*			Control the weather V1.0					*/
-/*			By Albert Seuba	- 042319					*/
-/********************************************************/
-/* Consultamos el tiempo actual (ciudad=Barcelona), 
-podemos variar la url pasando una variable desde inArguments y transformando country a su código */
-/*$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://dataservice.accuweather.com/currentconditions/v1/307297?apikey=aE0Mu6wczdfgTIZacsEksP0KBDAUYZjr",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1
-));
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-	$tempsbarcelona = json_decode($response);
-	$accuweather_temps = $tempsbarcelona[0]->{'WeatherText'};
-	echo $accuweather_temps;
-};*/
-	  echo 'sunny';
-?>
   </body>
 </html>
