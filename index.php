fd0fbbca1b6d2865cae7c419f398588b321d310d
seuba <49449750+seuba@users.noreply.github.com>
2019-06-04 14:24:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,32 @@
 				require(config, albert);
 			})();
 		</script>
-
+<?php
+/********************************************************/
+/*			Control the weather V1.0					*/
+/*			By Albert Seuba	- 042319					*/
+/********************************************************/
+/* Consultamos el tiempo actual (ciudad=Barcelona), 
+podemos variar la url pasando una variable desde inArguments y transformando country a su código */
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://dataservice.accuweather.com/currentconditions/v1/307297?apikey=aE0Mu6wczdfgTIZacsEksP0KBDAUYZjr",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+	$tempsbarcelona = json_decode($response);
+	$accuweather_temps = $tempsbarcelona[0]->{'WeatherText'};
+	echo $accuweather_temps;
+};
+?>
   </body>
 </html>
