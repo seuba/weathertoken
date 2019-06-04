81a4a0ebf465cb74dc546a40e72795b8a53e3181
seuba <49449750+seuba@users.noreply.github.com>
2019-06-04 14:15:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head></head>
+  <body>
+    <script src="https://albertseuba.herokuapp.com/blocksdk.js"></script>
+	  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js"></script> 
+	<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+	  <script type="text/javascript">
+			(function() {
+				var config = {
+					baseUrl: ''
+				};
+				var albert = [
+					'blocksdk',
+					'codi'
+				];
+				require(config, albert);
+			})();
+		</script>
+
+  </body>
+</html>
